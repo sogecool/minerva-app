c0e9ae4fcaf4cea17ec7f7bcd2eafc627a904e64
Aaron Hdez <ksamsarojam@gmail.com>
2024-03-07 16:44:40 -0800

    implementando los atributos id y name en el data class

--- app/src/main/java/minervapg/project/superheroapp/SuperHeroDataResponse.kt
+++ app/src/main/java/minervapg/project/superheroapp/SuperHeroDataResponse.kt
@@ -2,4 +2,12 @@ package minervapg.project.superheroapp
 
 import com.google.gson.annotations.SerializedName
 
-data class SuperHeroDataResponse(@SerializedName("response") val isWorking: String)
+data class SuperHeroDataResponse(
+    @SerializedName("response") val isWorking: String,
+    @SerializedName("results") val superheroes: List<SuperheroItemResponse>
+)
+
+data class SuperheroItemResponse(
+    @SerializedName("id") val superheroId: String,
+    @SerializedName("name") val name: String
+)
