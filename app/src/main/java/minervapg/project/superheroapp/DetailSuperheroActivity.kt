901af5f3081579f99b9c70915904fb5db105f0a8
Aaron Hdez <ksamsarojam@gmail.com>
2024-03-13 16:40:08 -0700

    terminando la app de superheros

--- app/src/main/java/minervapg/project/superheroapp/DetailSuperheroActivity.kt
+++ app/src/main/java/minervapg/project/superheroapp/DetailSuperheroActivity.kt
@@ -3,13 +3,17 @@ package minervapg.project.superheroapp
 import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
+import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import minervapg.project.R
 import minervapg.project.databinding.ActivityDetailSuperheroBinding
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import minervapg.project.R
+import kotlin.math.roundToInt
 
 class DetailSuperheroActivity : AppCompatActivity() {
 
@@ -24,6 +28,7 @@ class DetailSuperheroActivity : AppCompatActivity() {
         binding = ActivityDetailSuperheroBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val id: String = intent.getStringExtra(EXTRA_ID).orEmpty()
+        getSuperheroInformation(id)
     }
 
     private fun getSuperheroInformation(id: String) {
@@ -39,7 +44,32 @@ class DetailSuperheroActivity : AppCompatActivity() {
     }
 
     private fun createUI(superhero: SuperHeroDetailResponse) {
+        Picasso.get().load(superhero.image.url).into(binding.ivSuperhero)
+        binding.tvSuperheroName.text = superhero.name
+        prepareStats(superhero.powerstats)
+        binding.tvSuperheroRealName.text = superhero.biography.fullName
+        binding.tvPublisher.text = superhero.biography.publisher
 
+    }
+
+    private fun prepareStats(powerstats: PowerStatsResponse) {
+        updateHeight(binding.viewCombat, powerstats.combat)
+        updateHeight(binding.viewDurability, powerstats.durability)
+        updateHeight(binding.viewSpeed, powerstats.speed)
+        updateHeight(binding.viewStrength, powerstats.strength)
+        updateHeight(binding.viewIntelligence, powerstats.intelligence)
+        updateHeight(binding.viewPower, powerstats.power)
+
+    }
+
+    private fun updateHeight(view: View, stat: String){
+        val params = view.layoutParams
+        params.height = pxToDp(stat.toFloat())
+        view.layoutParams = params
+    }
+
+    private fun pxToDp(px:Float):Int{
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, px, resources.displayMetrics).roundToInt()
     }
 
     private fun getRetrofit(): Retrofit {
--- app/src/main/java/minervapg/project/superheroapp/SuperHeroDetailResponse.kt
+++ app/src/main/java/minervapg/project/superheroapp/SuperHeroDetailResponse.kt
@@ -4,7 +4,10 @@ import com.google.gson.annotations.SerializedName
 
 data class SuperHeroDetailResponse(
     @SerializedName("name") val name: String,
-    @SerializedName("powerstats") val powerstats: PowerStatsResponse
+    @SerializedName("powerstats") val powerstats: PowerStatsResponse,
+    @SerializedName("image") val image: SuperHeroImageDetail,
+    @SerializedName("biography") val biography: Biography
+
 )
 
 data class PowerStatsResponse(
@@ -14,4 +17,10 @@ data class PowerStatsResponse(
     @SerializedName("durability") val durability: String,
     @SerializedName("power") val power: String,
     @SerializedName("combat") val combat: String
+)
+
+data class SuperHeroImageDetail(@SerializedName("url") val url: String)
+data class Biography(
+    @SerializedName("full-name") val fullName: String,
+    @SerializedName("publisher") val publisher: String
 )
